6352b581938a54e339ba072c9e3fefdeffb1fa53
Tim Fuchs <tim.puhl@gmail.com>
2026-04-30 10:40:26 +0200

    feat(rag): add SCS Toolbox

--- 08-prototype-second-version/components/grounded-docs-mcp/index-docs.sh
+++ 08-prototype-second-version/components/grounded-docs-mcp/index-docs.sh
@@ -9,5 +9,6 @@ npx @arabold/docs-mcp-server@latest scrape "EXtra-geom" https://extra-geom.readt
 npx @arabold/docs-mcp-server@latest scrape "DAMNIT" https://damnit.readthedocs.io/en/latest/ --scope hostname --scrape-mode playwright
 npx @arabold/docs-mcp-server@latest scrape "Maxwell Infrastructure" https://docs.desy.de/maxwell/documentation/ --scope subpages --scrape-mode playwright
 npx @arabold/docs-mcp-server@latest scrape "Offline Calibration" https://calibration.pages.xfel.eu/pycalibration/ --scope subpages --scrape-mode playwright
+npx @arabold/docs-mcp-server@latest scrape "SCS Toolbox" https://scs.pages.xfel.eu/toolbox/ --scope subpages --scrape-mode playwright
 npx @arabold/docs-mcp-server@latest scrape "Technique-Oriented Docs FXE" https://dataanalysis.pages.xfel.eu/techniques-docs/fxe/ --scope subpages --scrape-mode playwright
 npx @arabold/docs-mcp-server@latest scrape "Technique-Oriented Docs SQS" https://dataanalysis.pages.xfel.eu/techniques-docs/sqs/ --scope subpages --scrape-mode playwright
